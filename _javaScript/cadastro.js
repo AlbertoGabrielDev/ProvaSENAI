29b92788574080dc7d7b19bb30a2db478943884e
Alberto Gabriel Martins Reis <1034601@ad.novomundo.com.br>
2023-01-02 09:59:29 -0300

    att js

--- _javaScript/cadastro.js
+++ _javaScript/cadastro.js
@@ -21,55 +21,62 @@ file.addEventListener('change', () => {
 });
 
 
-function validarCPF(cpf) {
+function validarCPF() {
 
-	cpf = cpf.replace(/[^\d]+/g, '');
-	if (cpf == '') {
+	// cpf = '04909331140'
+	var valor = document.querySelector("#valor").value;
+	console.log(document.querySelector("#valor").value)
+
+
+	// valor = '';
+	valor = valor.replace(/[^\d]+/g, '');
+	if (valor == '') {
 		alert("Erro")
 		return false
+	} else {
+		// Elimina CPFs invalidos conhecidos
+		if (valor.length != 11 ||
+			valor == "00000000000" ||
+			valor == "11111111111" ||
+			valor == "22222222222" ||
+			valor == "33333333333" ||
+			valor == "44444444444" ||
+			valor == "55555555555" ||
+			valor == "66666666666" ||
+			valor == "77777777777" ||
+			valor == "88888888888" ||
+			valor == "99999999999") {
+			alert("CPF Invalido")
+			return false;
+		}
 
+		// Valida 1o digito	
+		add = 0;
+		for (i = 0; i < 9; i++)
+			add += parseInt(valor.charAt(i)) * (10 - i);
+		rev = 11 - (add % 11);
+		if (rev == 10 || rev == 11)
+			rev = 0;
+		if (rev != parseInt(valor.charAt(9))) {
+			alert("CPF Invalido2")
+			return false;
+		}
+		// Valida 2o digito	
+		add = 0;
+		for (i = 0; i < 10; i++)
+			add += parseInt(valor.charAt(i)) * (11 - i);
+		rev = 11 - (add % 11);
+		if (rev == 10 || rev == 11)
+			rev = 0;
+		if (rev != parseInt(valor.charAt(10))) {
+			alert("nha")
+			return false;
+
+		}
+		alert("Certo")
+		return true;
 	};
-	// Elimina CPFs invalidos conhecidos	
-	if (cpf.length != 11 ||
-		cpf == "00000000000" ||
-		cpf == "11111111111" ||
-		cpf == "22222222222" ||
-		cpf == "33333333333" ||
-		cpf == "44444444444" ||
-		cpf == "55555555555" ||
-		cpf == "66666666666" ||
-		cpf == "77777777777" ||
-		cpf == "88888888888" ||
-		cpf == "99999999999") {
-		alert("CPF Invalido")
-		return false;
-	}
 
-	// Valida 1o digito	
-	add = 0;
-	for (i = 0; i < 9; i++)
-		add += parseInt(cpf.charAt(i)) * (10 - i);
-	rev = 11 - (add % 11);
-	if (rev == 10 || rev == 11)
-		rev = 0;
-	if (rev != parseInt(cpf.charAt(9))) {
-		alert("CPF Invalido2")
-		return false;
-	}
-	// Valida 2o digito	
-	add = 0;
-	for (i = 0; i < 10; i++)
-		add += parseInt(cpf.charAt(i)) * (11 - i);
-	rev = 11 - (add % 11);
-	if (rev == 10 || rev == 11)
-		rev = 0;
-	if (rev != parseInt(cpf.charAt(10))) {
-		alert("nha")
-		return false;
 
-	}
-	alert("aee")
-	return true;
 }
-cpf = "04909331140";
 // alert (validarCPF(cpf));
